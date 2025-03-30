5d14bd1979b6f553456ba51f618cc781b1672f2d
Pedro Basilio <phbasilio6903@gmail.com>
2025-03-30 14:21:11 -0300

    Feat: adicionando a verificação da existencia do arquivo no caminho

--- AtualizaTabelas.py
+++ AtualizaTabelas.py
@@ -10,3 +10,16 @@ caminho_pdf = os.path.join(base_dir, "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pd
 
 
 arquivo_csv = os.path.join(base_dir, "dados_extraidos_corrigidos.csv")
+
+
+cabecalho_salvo = None
+
+
+if not os.path.exists(caminho_pdf):
+    print(f"❌ Arquivo não encontrado: {caminho_pdf}")
+else:
+    with pdfplumber.open(caminho_pdf) as pdf:
+        with open(arquivo_csv, mode="w", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+
+  
