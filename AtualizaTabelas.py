088b6ef92fd7f82704550acf990322c338cf5906
Pedro Basilio <phbasilio6903@gmail.com>
2025-03-30 14:25:59 -0300

    feat:adicionando arquivos comprimidos e função que comprime

--- AtualizaTabelas.py
+++ AtualizaTabelas.py
@@ -1,19 +1,20 @@
 import pdfplumber
 import csv
 import os
+import zipfile
 
-
+# Diretório base
 base_dir = r"C:\ROBOS\AtualizaTabelasproced\anexos"
 
-
+# Caminho do PDF e do CSV
 caminho_pdf = os.path.join(base_dir, "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf")
-
-
 arquivo_csv = os.path.join(base_dir, "dados_extraidos_corrigidos.csv")
 
+# Caminho do ZIP final
+zip_path = os.path.join(base_dir, "Teste[Pedro Henrique Basilio].zip")
 
+# Variável para armazenar o cabeçalho e evitar duplicação
 cabecalho_salvo = None
-
 
 if not os.path.exists(caminho_pdf):
     print(f"❌ Arquivo não encontrado: {caminho_pdf}")
@@ -22,20 +23,26 @@ else:
         with open(arquivo_csv, mode="w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
 
-            
-          for pagina in pdf.pages:
-                tabelas = pagina.extract_table()  
+            for pagina in pdf.pages:
+                tabelas = pagina.extract_table()
 
                 if tabelas:
                     for i, linha in enumerate(tabelas):
-                        if i == 0:  
+                        if i == 0:
                             if cabecalho_salvo is None:
                                 # Substitui valores no cabeçalho
-                                cabecalho_salvo = [col.replace("OD", "Seg. Odontologica").replace("AMB", "Seg. Ambulatorial") for col in linha]
-                                writer.writerow(cabecalho_salvo) 
+                                cabecalho_salvo = [
+                                    col.replace("OD", "Seg. Odontologica").replace("AMB", "Seg. Ambulatorial")
+                                    for col in linha
+                                ]
+                                writer.writerow(cabecalho_salvo)
                         else:
-                            writer.writerow(linha) 
+                            writer.writerow(linha)
 
-    print(f"✅ Extração refinada concluída! Arquivo salvo em {arquivo_csv}")           
+    print(f"✅ Extração do arquivo concluída! Arquivo salvo em {arquivo_csv}")
 
-  
+    # Criar o arquivo ZIP
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(arquivo_csv, os.path.basename(arquivo_csv))
+
+    print(f"✅ Arquivo compactado com sucesso! Arquivo salvo em {zip_path}")
